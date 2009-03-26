0524183b7a76c28bb22a7d096415143739fad6ca
Daniel Schreiber <schreiber@esda.com>
2009-03-26 08:00:12 +0000

    Bugfix. h() ist nur im Templatekontext verfügbar
    git-svn-id: svn://svn.esda.local/trunk/esdaweb/vendor/plugins/scaffolding_esda@2823 28bfca7a-c10d-0410-b1fe-ac714baaa4f2

--- lib/esda/scaffolding/show.rb
+++ lib/esda/scaffolding/show.rb
@@ -4,7 +4,7 @@ module Esda::Scaffolding::Show
       @instance = model_class.find(params[:id])
       render_scaffold_tng "show"
     rescue ActiveRecord::RecordNotFound
-      render :inline=>"Datensatz mit ID #{params[:id].to_i} nicht gefunden", :status=>404
+      render :inline=>"Datensatz mit ID <%= params[:id].to_i %> nicht gefunden", :status=>404
     end
   end
 end
