383118a27e8b128de81d7032e1a8c037954fdc3d
schreiber <schreiber@28bfca7a-c10d-0410-b1fe-ac714baaa4f2>
2009-04-01 10:29:49 +0000

    Editor für has_and_belongs_to_many
    git-svn-id: svn://svn.esda.local/trunk/esdaweb/vendor/plugins/scaffolding_esda@2849 28bfca7a-c10d-0410-b1fe-ac714baaa4f2

--- lib/scaffolding_tng.rb
+++ lib/scaffolding_tng.rb
@@ -38,27 +38,79 @@ class ActionController::Base
       end
     end_eval
     if add_methods.include?(:browse)
-      #module_eval <<-"end_eval", __FILE__, __LINE__
         include Esda::Scaffolding::Browse
-      #end_eval
     end
     if add_methods.include?(:new)
-      #module_eval <<-"end_eval", __FILE__, __LINE__
         include Esda::Scaffolding::New
-      #end_eval
     end
     if add_methods.include?(:edit)
-      #module_eval <<-"end_eval", __FILE__, __LINE__
         include Esda::Scaffolding::Edit
-      #end_eval
     end
     if add_methods.include?(:show)
-      #module_eval <<-"end_eval", __FILE__, __LINE__
         include Esda::Scaffolding::Show
-      #end_eval
     end
     if add_methods.include?(:destroy)
       include Esda::Scaffolding::Destroy
+    end
+    if options[:habtm]
+      if options[:habtm].is_a?(Symbol)
+        options[:habtm] = [options[:habtm]]
+      end
+      include Esda::Scaffolding::Browse
+      klass = class_name.constantize
+      options[:habtm].each {|assoc_sym|
+        assoc = klass.reflect_on_association(assoc_sym)
+        next if assoc.nil?
+        module_eval <<-"end_eval", __FILE__, __LINE__
+          def edit_#{assoc.name}
+            @instance = model_class.find(params[:id])
+            @assoc = model_class.reflect_on_association(:#{assoc_sym})
+            render_scaffold_tng('edit_habtm')
+          end
+          def browse_associated_#{assoc.name}
+            @assoc = model_class.reflect_on_association(:#{assoc_sym})
+            t = Time.now
+            model = @assoc.klass
+            conditions = ['1=1']
+            condition_params = []
+
+            params_part = (params[:search][model.name.underscore.to_sym] rescue nil)
+            conditions, condition_params = build_conditions(model, params_part) if params_part
+            conditions << "#\{model.table_name}.\#{model.primary_key} IN (SELECT \#{@assoc.association_foreign_key} from \#{@assoc.options[:join_table]} WHERE \#{@assoc.primary_key_name} = ?)"
+            condition_params << params[:id]
+            handle_browse_data(model, conditions, condition_params)
+          end
+          def browse_unassociated_#{assoc.name}
+            @assoc = model_class.reflect_on_association(:#{assoc_sym})
+            t = Time.now
+            model = @assoc.klass
+            conditions = ['1=1']
+            condition_params = []
+
+            params_part = (params[:search][model.name.underscore.to_sym] rescue nil)
+            conditions, condition_params = build_conditions(model, params_part) if params_part
+            conditions << "#\{model.table_name}.\#{model.primary_key} NOT IN (SELECT \#{@assoc.association_foreign_key} from \#{@assoc.options[:join_table]} WHERE \#{@assoc.primary_key_name} = ?)"
+            condition_params << params[:id]
+            handle_browse_data(model, conditions, condition_params)
+          end
+          def add_#{assoc.name}
+            @instance = model_class.find(params[:id])
+            @assoc_inst = model_class.reflect_on_association(:#{assoc_sym}).klass.find(params[:add])
+            unless @instance.#{assoc.name}.include?(@assoc_inst)
+              @instance.#{assoc.name} << @assoc_inst
+            end
+            render :inline=>'OK'
+          end
+          def del_#{assoc.name}
+            @instance = model_class.find(params[:id])
+            @assoc_inst = model_class.reflect_on_association(:#{assoc_sym}).klass.find(params[:del])
+            if @instance.#{assoc.name}.include?(@assoc_inst)
+              @instance.#{assoc.name}.delete(@assoc_inst)
+            end
+            render :inline=>'OK'
+          end
+        end_eval
+      }
     end
   end
 
