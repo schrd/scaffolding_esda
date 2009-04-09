fbcb9e84a7edddb5a00ed34c5ab2583483ae98b3
Daniel Schreiber <schreiber@esda.com>
2009-04-09 12:11:31 +0000

    Clonefunktion tabs im show
    git-svn-id: svn://svn.esda.local/trunk/esdaweb/vendor/plugins/scaffolding_esda@2896 28bfca7a-c10d-0410-b1fe-ac714baaa4f2

--- lib/esda/scaffolding/show.rb
+++ lib/esda/scaffolding/show.rb
@@ -2,7 +2,16 @@ module Esda::Scaffolding::Show
   def show
     begin
       @instance = model_class.find(params[:id])
-      render_scaffold_tng "show"
+      @tab_associations = model_class.reflect_on_all_associations.find_all{|a| 
+        a.macro==:has_many and not a.options.has_key?(:through)
+      }.sort_by{|a| 
+        a.name.to_s.humanize
+      }
+      if request.xhr?
+        render_scaffold_tng "show_inline"
+      else
+        render_scaffold_tng "show"
+      end
     rescue ActiveRecord::RecordNotFound
       render :inline=>"Datensatz mit ID <%= params[:id].to_i %> nicht gefunden", :status=>404
     end
