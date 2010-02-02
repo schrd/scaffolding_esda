e25deba29c672d5a3b80bfa1333baf9319aa9853
schreiber <schreiber@28bfca7a-c10d-0410-b1fe-ac714baaa4f2>
2010-02-02 10:35:58 +0000

    add documentation to scaffold_value method add more sensible scaffolding for binary columns  - default is a download link  - it can be rendered as image tag if model method is implemented add download_column action so binary data can be retrieved
    git-svn-id: svn://svn.esda.local/trunk/esdaweb/vendor/plugins/scaffolding_esda@3608 28bfca7a-c10d-0410-b1fe-ac714baaa4f2

--- lib/esda/scaffolding/show.rb
+++ lib/esda/scaffolding/show.rb
@@ -22,4 +22,45 @@ module Esda::Scaffolding::Show
       render :inline=>"Datensatz mit ID <%= params[:id].to_i %> nicht gefunden", :status=>404
     end
   end
+
+  # This action is called for downloading binary data that is stored in a
+  # :binary column in the database If the scaffold_value method is called for a
+  # binary column (it is called by default in scaffolding), it generated a link
+  # to this action. 
+  # This action expects an :id and a :column parameter
+  #
+  # You can override the transmitted mime_type in the model:
+  # Given a column pdf_document you have to implement the following method:
+  # 
+  # class MyModel
+  #   def mime_type_for_pdf_document
+  #     "application/pdf"
+  #   end
+  # end
+  #
+  # It will send application/pdf instead of application/octet-stream
+  # 
+  def download_column
+    begin
+      column = params[:column]
+      if not model_class.columns_hash.has_key?(column)
+        render :inline=>"Spalte nicht gefunden", :status=>404
+        return
+      end
+      if model_class.columns_hash[column].type != :binary
+        render :inline=>"Spalte nicht gefunden", :status=>404
+        return
+      end
+      @instance = model_class.find(params[:id])
+      mime_type = "application/octet-stream"
+      if @instance.respond_to?("mime_type_for_#{column}")
+        mime_type = @instance.send("mime_type_for_#{column}")
+      end
+      send_data(@instance.send(column), :type=>mime_type)
+
+    rescue ActiveRecord::RecordNotFound
+      render :inline=>"Datensatz mit ID <%= params[:id].to_i %> nicht gefunden", :status=>404
+    end
+
+  end
 end
