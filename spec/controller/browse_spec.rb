0589f4040944bd7a80b7994985546f634b498649
Daniel Schreiber <schreiber@esda.com>
2010-02-03 14:10:50 +0000

    (split scaffolding_esda) add specs for browse_data
    browse_data generates json data for live_grid

--- spec/controller/browse_spec.rb
+++ spec/controller/browse_spec.rb
@@ -1,9 +1,9 @@
 require File.expand_path(File.join(File.dirname(__FILE__),'..','spec_helper.rb'))
 require File.expand_path(File.join(File.dirname(__FILE__),'..','common_controllers.rb'))
 
-describe "module Esda::Scaffolding::Browse" do
+describe "module Esda::Scaffolding::Browse", "browse action" do
   controller_name :customer
-  it "should call find without conditions" do
+  it "should render the browse template" do
     get :browse
     response.should render_template("browse")
     assigns[:model].should == Customer
@@ -14,11 +14,33 @@ describe "module Esda::Scaffolding::Browse" do
     get :browse, {"search"=>{"customer"=>{"customer_number"=>"123"}}}
     assigns[:model].should == Customer
     assigns[:extra_params].should == "search[customer][customer_number]=123&link=true"
+    response.should render_template("browse")
   end
 
   it "should not handle extra parameters for visible columns" do
     get :browse, {"search"=>{"customer"=>{"customer_number"=>"123"}}}
     assigns[:model].should == Customer
     assigns[:extra_params].should == "&link=true"
+    response.should render_template("browse")
   end
+end
+
+describe "module Esda::Scaffolding::Browse", "browse action" do
+  controller_name :order
+  before(:each) do
+    @order = mock_model(Order)
+  end
+  it "should call find without conditions if no parameters are specified" do
+    Order.should_receive(:find).with(:all, :limit=>50, :conditions=>["1=1"], :order=>nil, :include=>[], :offset=>0)
+    get :browse_data, {:limit=>50}
+    assigns[:model].should == Order
+  end
+
+  it "should call find with conditions" do
+    Order.should_receive(:scaffold_browse_fields).at_least(:once).and_return(%w(order_numer customer.customer_number))
+    Order.should_receive(:find).with(:all, :limit=>50, :conditions=>["1=1 AND customers.customer_number = ?", 12345], :order=>nil, :include=>[{:customer=>[]}], :offset=>0)
+    get :browse_data, {:limit=>50, :search=>{:order=>{"customer.customer_number"=>"12345"}}}  
+    response.content_type.should == "application/json"
+  end
+
 end
