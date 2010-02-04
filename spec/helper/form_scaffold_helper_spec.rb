a9e7f4f6df6766005fb0f180a798c292e65b7330
Daniel Schreiber <schreiber@esda.com>
2010-02-04 13:57:23 +0000

    (split scaffolding_esda) start spec for form_scaffold helper methods
    currently only for record_form and record_show methods. specs are not complete yet.

---
+++ spec/helper/form_scaffold_helper_spec.rb
@@ -0,0 +1,42 @@
+require File.expand_path(File.join(File.dirname(__FILE__),'..','spec_helper.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','common_controllers.rb'))
+describe Esda::Scaffolding::Helper::FormScaffoldHelper do
+  context "show_record" do
+    before(:each) do
+      @customer = Customer.new(:customer_number=>1234, :name=>"foobar")
+    end
+    it "should render all fields" do
+      get :foo
+      ret = helper.record_show(@customer)
+      ret.should have_tag("table")
+      ret.should contain("1234")
+      ret.should contain("foobar")
+    end
+    it "should render all fields except some" do
+      get :foo
+      ret = helper.record_show(@customer, :invisible_fields=>["name"])
+      ret.should have_tag("table")
+      ret.should contain("1234")
+      ret.should_not contain("foobar")
+    end
+  end
+  context "record_form" do
+    before(:each) do
+      @customer = Customer.new(:customer_number=>1234, :name=>"foobar")
+    end
+    it "should show all fields" do
+      get :foo
+      ret = helper.record_form(@customer)
+      ret.should have_xpath("//input[@value='1234']")
+      ret.should have_xpath("//input[@value='foobar']")
+      ret.should_not have_xpath("//input[@name='invisible_fields[]']")
+    end
+    it "should render all fields except invisible (hidden) fields" do
+      get :foo
+      ret = helper.record_form(@customer, :invisible_fields=>["name"])
+      ret.should have_xpath("//input[@value='1234' and @type='text']")
+      ret.should have_xpath("//input[@value='foobar' and @type='hidden' and @name='customer[name]']")
+      ret.should have_xpath("//input[@value='name' and @type='hidden' and @name='invisible_fields[]']")
+    end
+  end
+end
