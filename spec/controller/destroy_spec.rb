25ac360ddf68d1d22c43aa20b8a971bc734218c9
Daniel Schreiber <schreiber@esda.com>
2010-02-09 13:03:04 +0000

    (split scaffolding_esda) Spec for destroy action + documentation in model_extension

---
+++ spec/controller/destroy_spec.rb
@@ -0,0 +1,20 @@
+require File.expand_path(File.join(File.dirname(__FILE__),'..','spec_helper.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','common_controllers.rb'))
+
+describe "module Esda::Scaffolding::Destroy" do
+  controller_name :customer
+  before(:each) do
+    @cust = mock_model(Customer)
+    @cust.should_receive(:destroy)
+    Customer.should_receive(:find).with("12345").and_return(@cust)
+  end
+  it "should should remove data from the database" do
+    post :destroy, :id=>12345
+  end
+  it "should redirect according to redirect_to parameter" do
+    url = "/foo/bar"
+    post :destroy, :id=>12345, :redirect_to=>url
+    response.should redirect_to(url)
+  end
+end
+
