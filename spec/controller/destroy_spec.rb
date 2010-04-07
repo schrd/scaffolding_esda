39774ec96d809669ecb8f996c17a0d1a0f2b03b6
schreiber <schreiber@28bfca7a-c10d-0410-b1fe-ac714baaa4f2>
2010-04-07 15:28:39 +0000

    increase test coverage
    git-svn-id: svn://svn.esda.local/trunk/esdaweb/vendor/plugins/scaffolding_esda@3753 28bfca7a-c10d-0410-b1fe-ac714baaa4f2

--- spec/controller/destroy_spec.rb
+++ spec/controller/destroy_spec.rb
@@ -3,18 +3,24 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','common_controlle
 
 describe "module Esda::Scaffolding::Destroy" do
   controller_name :customer
-  before(:each) do
-    @cust = mock_model(Customer)
-    @cust.should_receive(:destroy)
-    Customer.should_receive(:find).with("12345").and_return(@cust)
+  context "test with data" do
+    before(:each) do
+      @cust = mock_model(Customer)
+      @cust.should_receive(:destroy)
+      Customer.should_receive(:find).with("12345").and_return(@cust)
+    end
+    it "should should remove data from the database" do
+      post :destroy, :id=>12345
+    end
+    it "should redirect according to redirect_to parameter" do
+      url = "/foo/bar"
+      post :destroy, :id=>12345, :redirect_to=>url
+      response.should redirect_to(url)
+    end
   end
-  it "should should remove data from the database" do
-    post :destroy, :id=>12345
-  end
-  it "should redirect according to redirect_to parameter" do
-    url = "/foo/bar"
-    post :destroy, :id=>12345, :redirect_to=>url
-    response.should redirect_to(url)
+  it "should render a 404 if called without id" do
+    post :destroy
+    response.response_code.should == 404
   end
 end
 
