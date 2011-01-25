c2966edcf7b1b295bc39de800d6e711b7ceace09
Daniel Schreiber <schreiber@esda.com>
2011-01-25 16:01:42 +0000

    (split scaffolding_esda) fix stupid bug

--- lib/esda/scaffolding/helper/scaffold_helper.rb
+++ lib/esda/scaffolding/helper/scaffold_helper.rb
@@ -302,14 +302,14 @@ module Esda::Scaffolding::Helper::ScaffoldHelper
   def header_above_tabs(instance)
     method_name = instance.class.name.underscore + "_header_above_tabs"
     if respond_to?(method_name.to_sym)
-      self.send(method_name.to_sym)
+      self.send(method_name.to_sym, instance)
     end
   end
 
   def footer_below_tabs(instance)
     method_name = instance.class.name.underscore + "_footer_below_tabs"
     if respond_to?(method_name.to_sym)
-      self.send(method_name.to_sym)
+      self.send(method_name.to_sym, instance)
     end
   end
 end
