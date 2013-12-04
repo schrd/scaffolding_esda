ce95df7089e46ecd218069beaa7bcd9b7c016582
Daniel Schreiber <schreiber@esda.com>
2013-12-04 15:25:50 +0000

    (split scaffolding_esda) add updated_by user

--- lib/esda/scaffolding/helper/history_helper.rb
+++ lib/esda/scaffolding/helper/history_helper.rb
@@ -1,3 +1,5 @@
+# coding: UTF-8
+
 module Esda::Scaffolding::Helper::HistoryHelper
   def records_show_history(records, options={})
     return "" if records.size == 0
@@ -7,6 +9,9 @@ module Esda::Scaffolding::Helper::HistoryHelper
     else
       model = record.class
       fields = record.class.scaffold_fields
+      if model.column_names.include?("updated_by")
+        fields << "updated_by"
+      end
       name_prefix = options[:name_prefix] # nil default
       fixed_fields = options[:fixed_fields] || []
       invisible_fields = options[:invisible_fields] || []
@@ -21,39 +26,44 @@ module Esda::Scaffolding::Helper::HistoryHelper
 
       log_id_field = model.table_name + '_log_id'
       middle_id = (records[records.size/2].send(log_id_field) rescue nil)
+      
 
       content_tag('div',
         content_tag('table',
           content_tag('tr',
-            content_tag('th', '') +
+            content_tag('th', h('')) +
             records.map{|rec|
               nav_after = ((rec.object_id == records.first.object_id and rec.attributes.has_key?(log_id_field)) ? 
-                           link_to("&larr;", :action=>'history', :id=>rec.id, :after=>middle_id) + "&nbsp;" : "")
+                           link_to("&larr;".html_safe, :action=>'history', :id=>rec.id, :after=>middle_id) + "&nbsp;".html_safe : "".html_safe)
               nav_before = (rec.object_id == records.last.object_id ? 
-                           "&nbsp;" + link_to("&rarr;", :action=>'history', :id=>rec.id, :before=>middle_id) : "")
+                           "&nbsp;".html_safe + link_to("&rarr;".html_safe, :action=>'history', :id=>rec.id, :before=>middle_id) : "".html_safe)
               ts_text = rec.respond_to?(:date_on) ?
                   h("#{scaffold_value(rec, :date_on)} - #{scaffold_value(rec, :date_off)}") :
                   scaffold_value(rec, :updated_at)
 
               content_tag('th', nav_after + ts_text + nav_before)
-            }.join
+            }.join.html_safe
           ) + 
           fields.map{|f|
             content_tag('tr',
               content_tag('th', scaffold_field_name(record, f)) +
               records.zip((0...records.size).to_a).map{|rec, idx|
                 begin
-                  if diff_fields[idx] and diff_fields[idx].include?(f)
-                    content_tag('td', scaffold_value(rec, f, false), :class=>'changed')
+                  if f == "updated_by"
+                    content_tag('td', h(User.find_by_id(record.updated_by).try(:login)))
                   else
-                    content_tag('td', scaffold_value(rec, f, false)) 
+                    if diff_fields[idx] and diff_fields[idx].include?(f)
+                      content_tag('td', scaffold_value(rec, f, false), :class=>'changed')
+                    else
+                      content_tag('td', scaffold_value(rec, f, false)) 
+                    end
                   end
                 rescue ActiveRecord::MissingAttributeError
-                  ""
+                  h("")
                 end
-              }.join
+              }.join.html_safe
             )
-          }.join,
+          }.join.html_safe,
           :class=>"record-show"
         )
       )
