ce4568f1d6c363052d7664ee5b06f2d36abd2cbd
Daniel Schreiber <schreiber@esda.com>
2014-03-24 12:21:04 +0100

    use FormData for file uploads

--- lib/esda/scaffolding/controller/recursive_creator.rb
+++ lib/esda/scaffolding/controller/recursive_creator.rb
@@ -15,7 +15,7 @@ module Esda::Scaffolding::Controller::RecursiveCreator
     if params_part
       found = params_part.find_all{|k,v| v.is_a?(String) || v.is_a?(Numeric)}
       found = Hash[*found.flatten]
-      params_part.find_all{|k,v| v.is_a?(File)||v.is_a?(Tempfile)}.each do |k,v|
+      params_part.find_all{|k,v| v.is_a?(File)||v.is_a?(Tempfile)||v.respond_to?(:read)}.each do |k,v|
         found[k] = v.read
       end
     else 
