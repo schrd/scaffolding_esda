7fa33b09d108807224986e5bc12a1279be2c0f2a
Daniel Schreiber <schrd@schreiber.homelinux.org>
2015-03-22 21:54:02 +0100

    add include to scaffolding routing helper unless it is already loaded

--- lib/generators/esda/scaffold_controller_generator.rb
+++ lib/generators/esda/scaffold_controller_generator.rb
@@ -6,6 +6,12 @@ class #{class_name}Controller < ApplicationController
   scaffold :#{file_name}
 end
     FILE
+    if File.readlines("config/routes.rb").grep(/include Esda::Scaffolding::Routing/).size == 0
+      prepend_to_file "config/routes.rb" do
+        "include Esda::Scaffolding::Routing\n# add a resourceful routes with actions necessary for scaffolding:\n#  scaffold_resource :product\n"
+      end
+    end
+
     route "scaffold_resource :#{file_name}"
   end
 end
