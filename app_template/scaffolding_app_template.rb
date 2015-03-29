ef12c2b9f7f45944138d608b8bbc8efe3c2c876b
Daniel Schreiber <schrd@schreiber.homelinux.org>
2015-03-29 22:28:04 +0200

    application template: move code executed after bundle to seperate generator
    Rails < 4.2 does not support after_bundle, so user has to invoke generator

--- app_template/scaffolding_app_template.rb
+++ app_template/scaffolding_app_template.rb
@@ -3,87 +3,17 @@ gem "gettext_i18n_rails"
 gem "jquery-ui-rails"
 gem "jquery-turbolinks"
 gem "handlebars_assets"
+gem 'therubyracer',  platforms: :ruby
 
-generate(:model, "user", "name:string")
-inject_into_class "app/models/user.rb", "User" do 
-<<RUBY
-  def self.current_user=(u)
-    @current_user=u
-  end
-  def self.current_user
-    @current_user
-  end
-
-  def has_privilege?(priv)
-    true
-  end
-  def has_any_privilege?(priv)
-    true
-  end
-RUBY
-end
-
-inject_into_class "app/controllers/application_controller.rb", "ApplicationController" do
-<<RUBY
-  before_filter :load_user
-
-  helper :scaffold_menu
-  layout 'application'
-
-  protected
-  def load_user
-    User.current_user = User.first
-  end
-RUBY
-end
-
-
-prepend_to_file "config/routes.rb" do
-  "include Esda::Scaffolding::Routing\n
-# add a resourceful routes with actions necessary for scaffolding:\n#  scaffold_resource :product\n"
-end
-route "root 'scaffold_index#index'"
-
-empty_directory 'locale'
-initializer 'fast_gettext.rb', <<-RUBY
-FastGettext.add_text_domain '#{@app_name}',:path=>'locale'
-FastGettext.default_text_domain = '#{@app_name}'
-FastGettext.default_available_locales = ['en']
-RUBY
-
-
-# configure assets
-
-insert_into_file "app/assets/javascripts/application.js", :after=>/\/\/= require jquery$/ do 
-<<END
-
-//= require jquery.turbolinks
-
-//= require jquery-ui
-//= require handlebars
-
+if Rails::VERSION::MAJOR == 4 and Rails::VERSION::MINOR < 2
+  puts <<END
+Your Rails version does not support after_bundle. 
+Invoke ``rails generate esda:setup_scaffolding`` in your project to setup scaffolding correctly.
 END
+else
+  after_bundle do
+    # do not use spring as it does not know about bundled gems yet
+    ENV["DISABLE_SPRING"]="1"
+    generate "esda:setup_scaffolding"
+  end
 end
-append_to_file "app/assets/javascripts/application.js" do
-<<END
-//= require scaffolding_esda
-END
-end
-
-insert_into_file "app/assets/stylesheets/application.css", :before=>/ \*\// do
-<<END
- *= require jquery-ui
- *= require scaffolding_esda
-END
-end
-
-append_to_file "db/seeds.rb", <<-RUBY
-User.create(name: "example user")
-RUBY
-
-create_file "app/controllers/user_controller.rb", <<-RUBY
-class UserController < ApplicationController
-  scaffold :user
-end
-RUBY
-route "scaffold_resource :user"
